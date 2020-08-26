abcbcd8c647eba55930d1d9d26c74ec62b5dad9e
saniikakulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2020-08-27 00:18:36 +0530

    popup

--- Script.js
+++ Script.js
@@ -27,3 +27,30 @@ function removeItem(item)
     var index = itemList.indexOf(item);
     itemList.splice(index,1);
 }
+
+function popup()
+{
+    document.getElementById('popupid2').style.visibility="hidden";
+    document.getElementById('popupid').style.visibility="visible";
+}
+function popupclose()
+{
+    document.getElementById('popupid').style.visibility="hidden";
+}
+function popupsignup()
+{
+    document.getElementById('popupid').style.visibility="hidden";
+    document.getElementById('popupid2').style.visibility="visible";
+}
+function popupsignupclose()
+{
+    document.getElementById('popupid2').style.visibility="hidden";
+}
+function menushow()
+{
+    document.getElementById('dropdownmenu').style.visibility="visible";
+}
+function menuhide()
+{
+    document.getElementById('dropdownmenu').style.visibility="hidden";
+}
